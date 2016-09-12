6f8c2624f13337eaa585bbec5105db4aae10568b
Anuj Dutta <anuj@andhapp.com>
2016-09-12 01:18:02 +0100

    Adds spec for loading rake task from the app.
    Removes guard as dependency.

--- lib/activerecord_sans_rails.rb
+++ lib/activerecord_sans_rails.rb
@@ -1,7 +1,7 @@
 require "activerecord_sans_rails/version"
 
 module ActiverecordSansRails
-  def self.root
-    File.dirname __dir__
+  def self.load_rake_tasks
+    Kernel.load "#{File.dirname(Kernel.__dir__)}/lib/activerecord_sans_rails/tasks/activerecord_sans_rails.rake"
   end
 end
--- spec/activerecord_sans_rails_spec.rb
+++ spec/activerecord_sans_rails_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe ActiverecordSansRails do
+
   it 'has a version number' do
     expect(ActiverecordSansRails::VERSION).not_to be nil
   end
+
+  context 'load_rake_tasks' do
+
+    it 'makes correct calls' do
+      allow(Kernel).to receive(:__dir__) { "/rootDir" }
+      allow(File).to receive(:dirname).with("/rootDir") { "/rootDir" }
+      allow(Kernel).to receive(:load).
+        with("/rootDir/lib/activerecord_sans_rails/tasks/activerecord_sans_rails.rake")
+
+      ActiverecordSansRails::load_rake_tasks
+    end
+
+  end
+
 end
